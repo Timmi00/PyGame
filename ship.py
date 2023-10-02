a6b96b8b517ea7a44e8d0cad90e47ec9f51c5b03
Timmi00 <timmi00@tut.by>
2023-10-02 21:55:30 +0300

    Refactoring

--- ship.py
+++ ship.py
@@ -19,10 +19,13 @@ class Ship:
 
         # Сохранение вещественной координаты центра корабля
         self.x = float(self.rect.x)
+        self.y = float(self.rect.y)
 
         # Перемещение корабля.
         self.moving_right = False
         self.moving_left = False
+        self.moving_up = False
+        self.moving_down = False
 
     def update(self):
         """Update ship x-y"""
@@ -30,9 +33,14 @@ class Ship:
             self.x += self.settings.ship_speed
         if self.moving_left and self.rect.left > 0:
             self.x -= self.settings.ship_speed
+        if self.moving_up and self.rect.top > self.screen_rect.top:
+            self.y -= self.settings.ship_speed
+        if self.moving_down and self.rect.bottom < self.screen_rect.bottom:
+            self.y += self.settings.ship_speed
 
         # Обновление атрибута rect на основании self.x
         self.rect.x = self.x
+        self.rect.y = self.y
 
     def blitme(self):
         """Draw the ship at its current location."""
